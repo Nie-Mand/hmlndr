196c6f378293854bd3aa919b71c5a2a065e3c6e6
Nie-Mand <chill.out.x19@gmail.com>
2023-03-27 01:00:56 +0100

    hello world

---
+++ src/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav className="sticky top-0 mx-4 md:mx-20 xl:mx-40 py-6">
+            <h1 className="text-xl font-black">
+                HMLNDR
+            </h1>
+        </nav>
+    )
+}
